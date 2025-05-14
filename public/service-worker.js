54601242bec68a8036b5664db9351b523a8fd8af
Gunadizz <41579215+gunadizz@users.noreply.github.com>
2025-05-14 14:13:57 +0800

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('femmeiacloth-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
